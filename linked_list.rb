ea311df13226075431e3f4db28e77da6d4204152
Omar <omar.fayyaz@ryerson.ca>
2024-10-04 22:20:35 -0400

    finished head, tail, at, contains? functions

--- linked_list.rb
+++ linked_list.rb
@@ -26,4 +26,36 @@ class LinkedList
     return counter if node.tail.nil?
     size(node.tail, counter+1)
   end
+
+  def head
+    @head.head
+  end
+
+  def tail
+    @tail.head
+  end
+
+  def at(index)
+    if index > size - 1
+      return "Does not exist."
+    end
+    for a in 0..index do
+      if a == index
+        return @head.head
+      end
+      @head = @head.tail
+    end
+  end
+
+  def pop
+    @tail.head = nil
+  end
+
+  def contains?(val)
+    until @head.tail.nil?
+      return true if @head.head == val
+      @head = @head.tail
+    end
+    return false
+  end
 end
--- main.rb
+++ main.rb
@@ -6,4 +6,10 @@ a.append("apple")
 a.append("banana")
 a.prepend("cat")
 p a.size()
+p a.head
+p a.tail
+p a.at(3)
 p a
+a.pop
+p a
+p a.contains?("banana")
